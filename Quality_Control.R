f34c075eeb7e6d70b8193f3daae452ba6d894488
EiriniTryp <ei.tripidaki@gmail.com>
2024-03-06 16:50:35 +0100

    Load meteorological data

--- Quality_Control.R
+++ Quality_Control.R
@@ -15,3 +15,32 @@ install.packages("ggplot2")
 library(dplyr)
 library(sf)
 library(ggplot2)
+
+#Set working Directory
+setwd("XXXXX")
+
+#Load meteorological stations data
+csv_directory <- "XXXXX"
+csv_files <- list.files(csv_directory,pattern = "\\.csv$", full.names = TRUE)
+
+#Load study Area
+StudyArea <- st_read("C:/Users/e.trypidaki/OneDrive - CREAF/Escritorio/Data/EbroLimits/buffered25km.geojson", promote_to_multi = FALSE)
+
+# List all the csv files with meteorological data
+# Select variables
+read_weather_data <- function(csv_files) {
+  data <- lapply(csv_files, function(csv_file) {
+    df <- read.csv(csv_file)
+    df$Station_Name <- as.character(df$Station_Name)
+    df$Source <- basename(csv_file)
+    df$Year <- as.numeric(df$Year)
+    df <- select(df,"Station_Name","Year","Month","Station_Altitude","Precipitacion.mm",
+                 "Tmean.C","Tmin.C","Tmax.C","X","Y","Source")
+    return(df)
+  })
+  data <- bind_rows(data)
+  return(data)
+}
+# Load meteorological data
+df <- read_weather_data(csv_files)
+
