948301e2e9d06aef3426518f752134c62e2345ed
EiriniTryp <ei.tripidaki@gmail.com>
2024-03-06 16:14:22 +0100

    Add libraries

---
+++ Quality_Control.R
@@ -0,0 +1,17 @@
+# Eirini Trypidaki 
+# e.trypidaki@creaf.uab.cat
+
+## Quality control meteorological station data ##
+#Connect the project to Github
+library(usethis)
+use_github() # a repository will be created in Github
+
+##Install libraries
+install.packages("dplyr")
+install.packages("sf")
+install.packages("ggplot2")
+
+##Load libraries
+library(dplyr)
+library(sf)
+library(ggplot2)
