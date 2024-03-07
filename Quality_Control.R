26119e353d957e7e1d516f703235962e1f93241f
EiriniTryp <ei.tripidaki@gmail.com>
2024-03-07 12:59:08 +0100

    Add filtering by year

--- Quality_Control.R
+++ Quality_Control.R
@@ -51,16 +51,10 @@ read_weather_data <- function(csv_files) {
 }
 # Load meteorological data
 df <- read_weather_data(csv_files)
-
-
-# Creating base data frames 
-stationID_list <- unique(df$Station_Name)
 # Transform Year and Month to YYYYMM date format
 df$YYYYMMdate <- as.character(paste0(df$Year, "-", sprintf("%02d", df$Month), "-15"))
 
-ShortSeries <- data.frame(station=character(), initial_date=Date(), end_date=Date(), series_years_length=numeric())
-StationHighNApc <- data.frame(station = character(), NApc = numeric())
-StationNApc <- data.frame(station = character(), NApc = numeric())
+
 
 #_______________________________________________________________________________________
                   
@@ -69,11 +63,27 @@ StationNApc <- data.frame(station = character(), NApc = numeric())
 THRESHOLD_serieslength <- 1 # minimum length of a series
 THRESHOLD_NApc <- 30 # Maximum NA%
 
-for (station in "82") {
-  subset_data <- df[df$Station_Name == "A137", ]
 
-  # 1. Series length threshold____________________________________________________
+
+perform_quality_control_variable <- function(df, variable, THRESHOLD_serieslength,THRESHOLD_NApc) {
+  quality_control_results <- list(ShortSeries = data.frame(Station_Name = character(), 
+                                                           Initial_date = as.Date(character()), 
+                                                           End_date = as.Date(character()), 
+                                                           Series_years_length = numeric()),
+                                  StationHighNApc = data.frame(Station_Name = character(), NA_percentage = numeric()))
   
+  #Create list with the stations name
+  stationID_list <- unique(df$Station_Name)
+  
+  for (station in stationID_list) {
+    subset_data <- df[df$Station_Name == station, ]
+    
+  # Filter data to specific variable
+  subset_data <- subset_data %>% filter(!is.na(.data[[variable]]))
+                                        
+  # 1. Series length threshold____________________________________________________
+  # Check if there are valid dates for that variable
+  if (any(!is.na(subset_data$YYYYMMdate))) {
   # Find the earliest and oldest dates
   earliest_date <- min(subset_data$YYYYMMdate, na.rm = TRUE)
   oldest_date <- max(subset_data$YYYYMMdate, na.rm = TRUE)
@@ -81,10 +91,12 @@ for (station in "82") {
   # Calculate the difference in years between the dates
   difference_in_years <- as.numeric(difftime(oldest_date, earliest_date, units = "weeks") / 52.1775)
   
-  # Create a sequence of all dates between the minimum and maximum dates for the station
+  # Create a sequence of all the dates between the minimum and maximum dates for the station
     all_dates <- seq(from = as.Date(earliest_date), to = as.Date(oldest_date), by = "1 month") 
-  # Convert all_dates to character format
+    
+  # Convert all_dates to character format (needed because in df date is character)
     all_dates <- as.character(all_dates)
+    
   # Use the complete function to fill in missing dates
     subset_data <- subset_data %>%
       complete(YYYYMMdate = all_dates)
@@ -93,28 +105,33 @@ for (station in "82") {
   if (difference_in_years < THRESHOLD_serieslength) {
     print(paste("Time series for", station, "is shorter than", THRESHOLD_serieslength, "year."))
     info <- c(station, earliest_date, oldest_date, difference_in_years)
-    ShortSeries <- rbind(ShortSeries, info)
+    quality_control_results$ShortSeries <- rbind(quality_control_results$ShortSeries, info)
   } 
+    # 2. Number of NA values____________________________________________________
+    
+    # Calculate the number of NA values in the specified variable
+    num_na <- sum(is.na(subset_data[[variable]]))
+    # Calculate the total number of values
+    total_values <- length(all_dates)
+    # Calculate the percentage of NA values
+    percent_na <- (num_na / total_values) * 100
+
+    # Check if the % of NA values is greater than the Threshold %NA
+    if (percent_na > THRESHOLD_NApc) {
+      print(paste(station, "has more than", THRESHOLD_NApc, "% NA values."))
+      info <- c(station, percent_na)
+      quality_control_results$StationHighNApc <- rbind(quality_control_results$StationHighNApc, info)
+    }
+  } else {
+    print(paste("No valid dates for", station))
+  }
+  }
   
-  # 2.. Number of NA values. Who complete are the series. 
-  
-  # Calculate the number of NA values in the PREC column
-  num_na <- sum(is.na(df$Precipitacion.mm))
-  # Calculate the total number of values in the PREC column
-  total_values <- length(all_dates)
-  
-  # Calculate the percentage of NA values
-  percent_na <- (num_na / total_values) * 100
-  StationNAresult <- c(station, percent_na)
-  StationNApc <- rbind(StationNApc, StationNAresult)
-  
-  # Check if the % of NA values is greater than the Threshold %NA
-  if (percent_na > THRESHOLD_NApc) {
-    print(paste(station, "have more than", THRESHOLD_NApc, "% NA values."))
-    StationHighNApc <- rbind( StationHighNApc,StationNAresult )
-  } 
-  
+  return(quality_control_results)
 }
+
+##
+quality_control_precipitation<- perform_quality_control_variable(df, "Precipitacion.mm", 1, 30)
 
 #_______________________________________________________________________________________
 
@@ -132,14 +149,14 @@ header_line <- paste("List of stations with a time series shorter than the thres
 write(header_line, "Quality Control.txt", append = TRUE)
 
 # Write the extracted data frame to a text file
-write.table(ShortSeries, file = "Quality Control.txt", append = TRUE,col.names=!file.exists("Quality Control.txt"))
+write.table(quality_control_results$ShortSeries, file = "Quality Control.txt", append = TRUE,col.names=!file.exists("Quality Control.txt"))
 
 # Write NA% info
 header_line <- paste("List of stations with a NA% higher than the threshold:", THRESHOLD_NApc, "%.")
 write(header_line, "Quality Control.txt", append = TRUE)
 
 # Write the extracted data frame to a text file
-write.table(StationHighNApc, file = "Quality Control.txt",   append = TRUE,col.names=!file.exists("Quality Control.txt"))
+write.table(quality_control_results$StationHighNApc, file = "Quality Control.txt",   append = TRUE,col.names=!file.exists("Quality Control.txt"))
 
 # No need to explicitly return a message, it will be returned automatically
 "Extraction complete. Check Quality Control.txt files."
@@ -165,11 +182,8 @@ writeData(wb, sheet = "NA Percentage", x = "QUALITY CONTROL REPORT", startCol = 
 writeData(wb, sheet = "NA Percentage", x = "List of stations with an NA%.", startCol = 1, startRow = 2)
 
 # Write Series length info to Excel
-writeData(wb, sheet = "Series Length", x = ShortSeries, startRow = 3)
-
-# Write NA% info to Excel
-writeData(wb, sheet = "High NA Percentage", x = StationHighNApc, startRow = 3)
-writeData(wb, sheet = "NA Percentage", x = StationNApc, startRow = 3)
+writeData(wb, sheet = "Series Length", x = quality_control_results$ShortSeries, startRow = 3)
+writeData(wb, sheet = "High NA Percentage", x = quality_control_results$StationHighNApc, startRow = 3)
 
 # Check if the file exists
 if (file.exists("Quality Control.xlsx")) {
@@ -182,23 +196,6 @@ saveWorkbook(wb, "Quality Control.xlsx")
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #_______________________________________________________________________________________
 
 ##### Filter data by year ####
@@ -206,17 +203,17 @@ saveWorkbook(wb, "Quality Control.xlsx")
 ### Keep stations before 2000 with 5 or more years of data ##
 ### After 2000 with 1 or more years of data ##
 ### Remove Stations where all the values are NA ##
-## Precipitation ##
-prec_filtered <- prec %>%
-  group_by(Station_Name) %>%
-  filter(!all(is.na(Precipitacion.mm))) %>%
-  filter((Year < 2000 & n_distinct(Year) >= 5) | (Year >= 2000 & n_distinct(Year) >= 1)) %>%
-  ungroup()
 
+filter_year <- function(df, station, variable, year_variable, threshold_year, min_obs_below_threshold, min_obs_above_threshold) {
+  df %>%
+    group_by({{ station }}) %>%
+    filter(!all(is.na({{ variable }}))) %>%
+    filter(({{ year_variable }} < threshold_year & n_distinct({{ year_variable }}) >= min_obs_below_threshold) |
+             ({{ year_variable }} >= threshold_year & n_distinct({{ year_variable }}) >= min_obs_above_threshold)) %>%
+    ungroup()
+}
 
-## Temperature ##
-tempe_filtered  <- tempe %>%
-  group_by(Station_Name) %>%
-  filter(!all(is.na(Tmean.C))) %>%
-  filter((Year < 2000 & n_distinct(Year) >= 5) | (Year >= 2000 & n_distinct(Year) >= 1)) %>%
-  ungroup()
+#Filter
+prec_filtered <- filter_year(df, Station_Name, Precipitacion.mm, Year, 2000, 5, 1)
+tempe_filtered  <- filter_year(df, Station_Name, Tmean.C, Year, 2000, 5, 1)
+
